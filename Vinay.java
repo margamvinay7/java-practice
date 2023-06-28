0ff7e06f6ba036185828dee7a8ba9dd54b97ea89
margamvinay7 <margamvinay77@gmail.com>
2023-06-28 23:30:30 +0530

    new branch

---
+++ Vinay.java
@@ -0,0 +1,24 @@
+class Inheritance {
+    public void in1(){
+        int i=1;
+        System.out.println(i);
+    }
+}
+class Mani extends Inheritance{
+    public void in2(){
+        int j=7;
+        System.out.println(j);
+    }
+}
+class Vinay extends Mani {
+    public void in3(){
+        int k=3;
+        System.out.println(k);
+    }
+    public static void main(String[] args){
+Vinay obj = new Vinay();
+obj.in3();
+obj.in2();
+obj.in1();
+    }
+}
